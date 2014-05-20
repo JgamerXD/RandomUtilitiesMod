5b5eb0fe6be179777d5fe47f6bfa2b1c8f3c55c6
JgamerXD <JgamerLP@gmail.com>
2014-05-20 18:01:13 +0200

    Modifiers

--- src/randomutilitiesmod/com/github/JgamerXD/RandomUtilitiesMod/Modifier/Modifier.java
+++ src/randomutilitiesmod/com/github/JgamerXD/RandomUtilitiesMod/Modifier/Modifier.java
@@ -1,11 +1,16 @@
 package com.github.JgamerXD.RandomUtilitiesMod.Modifier;
 
+import com.github.JgamerXD.RandomUtilitiesMod.Items.ItemModifierFocus;
+
 import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.item.ItemStack;
 
 public class Modifier {
+	
+	ItemStack item;
+	ItemStack focus;
 
-	Modifier(ItemStack item)
+	Modifier(ItemStack focus,ItemStack item)
 	{
 		
 	}
@@ -15,9 +20,9 @@ public class Modifier {
 	void onHit(ItemStack item,EntityLivingBase entityHit){}
 	
 	//Entity
-	void onHit(EntityModified entity,EntityLivingBase entityHit){}	
-	void entityUpdate(EntityModified entity){}
-	void entityHitGround(EntityModified entity){}
+	void onHit(IEntityModified entity,EntityLivingBase entityHit){}	
+	void entityUpdate(IEntityModified entity){}
+	void entityHitGround(IEntityModified entity){}
 	
 	
 }
